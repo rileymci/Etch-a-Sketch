22bef7b1c5f3057f6de1fd6364d7b24e36a7bc7c
rileymci <riley.mcilwain@connect.qut.edu.au>
2023-11-28 18:12:31 +1000

    Switch between rainbow mode and black mode clr btn

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 var id = 0;
 var rangeValue = 16;
+var boxHeight = "32px";
+var boxWidth = "32px";
 
-document.getElementById("myRange").oninput = (e) => {
-    rangeValue = parseInt(e.target.value);
-    document.getElementById("demo").innerText = rangeValue + " x " + rangeValue;
+var rainbow = false;
+
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    return bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
 }
-
 
 function clearBox(){
-    
+    var child = maindiv.lastElementChild;
+    while(child) {
+        maindiv.removeChild(child);
+        child = maindiv.lastElementChild;
+    }
+    createGrid();
 }
 
-function createBox(element) {
+function createGrid() {
+
+    var HWcalc = 512 / rangeValue;
+    boxHeight = HWcalc.toString() + "px";
+    boxWidth = HWcalc.toString() + "px";
+
+    for (i = 0; i < rangeValue; i++){ //each row
+        var row = createRow(maindiv);
+        for (b = 0; b < rangeValue; b++){ //each column
+            createBox(row, boxHeight, boxWidth);
+        }
+    }
+}
+
+function rainbowMode(){
+    var list = maindiv.children;
+    console.log(list);
+}
+
+function createBox(element, Height, Width) {
     var div = document.createElement("div");
     div.id = 'box';
-    div.style.height = "32px";
-    div.style.width = "32px";
+    div.style.height = Height;
+    div.style.width = Width;
     div.style.flex = "1";
     div.addEventListener("mouseover", function(){
-        div.style.backgroundColor = "black";
+        if(rainbow){
+            div.style.backgroundColor = random_bg_color();
+        }
+        else{
+            div.style.backgroundColor = "black";
+        }
+
     });
     element.append(div);
 }
@@ -30,11 +67,37 @@ function createRow(main){
     return div;
 }
 
+const button1 = document.querySelector('#rainbowBtn');
+const button2 = document.querySelector('#blackBtn');
+const button3 = document.querySelector('#clearBtn');
+    
+    button1.addEventListener('click', () => {
+        rainbow = true;
+        button1.style.color = "green";
+        button2.style.color = "black";
+    });
+    button2.addEventListener('click', () => {
+        rainbow = false;
+        button2.style.color = "green";
+        button1.style.color = "black";
+    });
+    button3.addEventListener('click', () => {
+        clearBox();
+    });
+
+
 const maindiv = document.querySelector(".main");
+
+document.getElementById("myRange").oninput = (e) => {
+    rangeValue = parseInt(e.target.value);
+    document.getElementById("inputSize").innerText = rangeValue + " x " + rangeValue;
+}
 
 for (i = 0; i < rangeValue; i++){ //each row
     var row = createRow(maindiv);
     for (b = 0; b < rangeValue; b++){ //each column
-        createBox(row);
+        createBox(row, boxHeight, boxWidth);
     }
 }
+
+rainbowMode();
